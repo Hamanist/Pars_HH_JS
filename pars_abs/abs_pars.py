20bc356efcdc340624efc2ed0e248a2f75d9ba5b
Hamanist <hamanist@gmail.com>
2024-01-13 15:52:24 +0300

    added a parsing class

---
+++ pars_abs/abs_pars.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+
+class VacancyParsing(ABC):
+    """
+    Абстрактный класс для парсинга HH и SJ
+    """
+
+    @abstractmethod
+    def get_vacancies(self):
+        """
+        :return: Получаем вакансии
+        """
+        pass
+
+    @abstractmethod
+    def _pars(self):
+        """
+        :return: Список вакансий
+        """
+        pass
